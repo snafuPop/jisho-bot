846faeb633b19159956d294d10886bcc0e20cae4
이이레 <krbingpop@gmail.com>
2021-11-03 18:47:20 -0400

    Update jisho.py

--- modules/jisho.py
+++ modules/jisho.py
@@ -116,7 +116,7 @@ class Language(commands.Cog):
       count += 1
 
     paginated_embed = Paginator(bot = self.bot, ctx = ctx, pages = embeds, authorOnly = True)
-    await paginated_embed.start()
+    await paginated_embed.run()
     #await ctx.send(embed = embed)
 
 
